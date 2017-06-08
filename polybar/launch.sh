e6cee60813691702c4994b27976837dc1cf4607e
aolwas <maxime.cottret@gmail.com>
2017-06-08 20:13:42 +0200

    backup commit

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar example &
+# Launch bars
+polybar eDP1 &
+IS_HDMI1_ACTIVE = `xrandr --listactivemonitors | grep 'HDMI1'`
+
+if [ -n $IS_HDMI1_ACTIVE ]; then
+    polybar HDMI1 &
+fi
 
 echo "Bars launched..."
