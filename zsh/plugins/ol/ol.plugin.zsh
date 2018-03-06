b278af1bc39b7d51d7230d800ab66eada6e274e4
Maxime Cottret <maxime.cottret@gmail.com>
2018-03-06 16:10:45 +0100

    use docker ui > 1.13

--- zsh/plugins/ol/ol.plugin.zsh
+++ zsh/plugins/ol/ol.plugin.zsh
@@ -2,7 +2,7 @@
 # aliases
 #
 
-alias ol='docker run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/ol/olclient "$@"'
-alias ol-doc-make='docker run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools make'
-alias ol-doc-bootstrap='docker run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools ol-doc-bootstrap'
-alias ol-doc-make-old='docker run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools:old make'
+alias ol='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/ol/olclient "$@"'
+alias ol-doc-make='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools make'
+alias ol-doc-bootstrap='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools ol-doc-bootstrap'
+alias ol-doc-make-old='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools:old make'
