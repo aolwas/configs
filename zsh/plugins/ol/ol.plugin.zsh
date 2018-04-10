cb19b639f92033807512d094289feb5be6e33291
Maxime Cottret <maxime.cottret@gmail.com>
2018-04-10 16:11:09 +0200

    minor update

--- zsh/plugins/ol/ol.plugin.zsh
+++ zsh/plugins/ol/ol.plugin.zsh
@@ -3,6 +3,8 @@
 #
 
 alias ol='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/ol/olclient "$@"'
-alias ol-doc-make='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools make'
-alias ol-doc-bootstrap='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools ol-doc-bootstrap'
+alias ol-slides-make='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools make'
+alias ol-slides-bootstrap='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools ol-slides-bootstrap'
 alias ol-doc-make-old='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools:old make'
+alias ol-doc-bootstrap-old='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools ol-doc-bootstrap'
+alias ol-slides-pdf='docker container run --rm -v $PWD:/data -w /data --entrypoint="" objectiflibre/decktape node /decktape/decktape.js --no-sandbox -s 1920x1080 -p 100 "$@"'
