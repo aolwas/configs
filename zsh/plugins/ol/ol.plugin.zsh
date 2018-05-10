cba9f7d8b21a6ead57561f76bc021140d9779b4c
Maxime Cottret <maxime.cottret@gmail.com>
2018-05-10 11:13:16 +0200

    minor update

--- zsh/plugins/ol/ol.plugin.zsh
+++ zsh/plugins/ol/ol.plugin.zsh
@@ -1,10 +1,11 @@
 #
+# FError: EOF when reading a line
 # aliases
 #
 
 alias ol='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/ol/olclient "$@"'
 alias ol-slides-make='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools make'
-alias ol-slides-bootstrap='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools ol-slides-bootstrap'
+alias ol-slides-bootstrap='docker container run -ti --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools ol-slides-bootstrap'
 alias ol-doc-make-old='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools:old make'
-alias ol-doc-bootstrap-old='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools ol-doc-bootstrap'
+alias ol-doc-bootstrap-old='docker container run -ti --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools:old ol-doc-bootstrap'
 alias ol-slides-pdf='docker container run --rm -v $PWD:/data -w /data --entrypoint="" objectiflibre/decktape node /decktape/decktape.js --no-sandbox -s 1920x1080 -p 100 "$@"'
