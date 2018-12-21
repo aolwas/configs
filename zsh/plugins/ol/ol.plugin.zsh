cd268adcc300e52ece43feaefd4094e1aeb90447
Maxime Cottret <maxime.cottret@gmail.com>
2018-12-21 17:37:57 +0100

    backup before pc change

--- zsh/plugins/ol/ol.plugin.zsh
+++ zsh/plugins/ol/ol.plugin.zsh
@@ -3,6 +3,7 @@
 # aliases
 #
 
+alias os-client='docker container run --rm -it -v $HOME:/home/user -w /home/user aolwas/openstack-client "$@"'
 alias ol='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/ol/olclient "$@"'
 alias ol-slides-make='docker container run --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools make'
 alias ol-slides-bootstrap='docker container run -ti --rm -v $PWD:/data -w /data registry.gitlab.objectif-libre.com/apps/doc-tools ol-slides-bootstrap'
